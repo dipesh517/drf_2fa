a4e3f4f493cdceb5660ab77da2f49c146ea8e94c
Pradip <pradip@savitriya.com>
2022-07-25 17:21:22 +0545

    added jwt authentication

--- auth/apps.py
+++ auth/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
-
 class AuthConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auth'
+    name = "auth"
+    verbose_name = "Authentication"
+    label = "custom_auth"
+
--- drf_2fa_demo/urls.py
+++ drf_2fa_demo/urls.py
@@ -1,21 +1,37 @@
-"""drf_2fa_demo URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+from auth.urls import urlpatterns_auth
+from auth.views import (
+    PhoneVerificationView,
+    PhoneRegistrationView,
+    AuthyTokenVerifyView,
+    CustomTokenObtainPairView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    # 2FA
+    path(
+        "api/2fa/phone-verify/",
+        PhoneVerificationView.as_view(),
+        name="2fa_phone_verify",
+    ),
+    path(
+        "api/2fa/phone-register/",
+        PhoneRegistrationView.as_view(),
+        name="2fa_register_phone",
+    ),
+    path(
+        "api/2fa/token-verify/", AuthyTokenVerifyView.as_view(), name="2fa_token_verify"
+    ),
+    # JWT authentication uris for Rest Api
+    path("api/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # Application Rest Api uris
+    path("api/", include((urlpatterns_auth, "custom_auth"))),
 ]
